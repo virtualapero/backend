1098d8fdc44c31e057653a25ebd09f6fd82b3f75
Que2004 <quentin-schuster@web.de>
2020-12-31 00:12:21 +0100

    add docs (OPENAPI)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {version} from "../package.json"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+      .setTitle("#virtualapero")
+      .setVersion(version)
+      .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("", app, document)
+
   await app.listen(8000);
 }
 
